4ae8d99c1e610bda599389c9d87200fc69ed1b73
Dusty White <dusty.white@gmail.com>
2024-10-06 19:41:54 -0500

    Update testDataLength.py

--- tests/testDataLength.py
+++ tests/testDataLength.py
@@ -19,4 +19,4 @@ lego2019 = pd.read_csv("lego2019.csv")
 class testCases(unittest.TestCase):
 
     def testLength(self):
-      self.assertTrue(lego2019.shape[0]==849, "Your data set does not have 849 entries scraped from brickset.com.")
+      self.assertTrue(lego2019.shape[0]>25, "Your data set does not collect more than one page from brickset.com.")
